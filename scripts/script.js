706733fc85054d406c693c33d01472b5dacf2c91
Kaisar <nasipolla2019@mail.ru>
2023-04-10 04:11:42 +0500

    end styles galleries

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,21 @@
 $(document).ready(function (){
-    // $('.booking-types').slick({
-    //     slidesToShow: 3,
-    //     slidesToScroll: 1,
-    //     dots: true,
-    //     speed: 1000,
-    //     adaptiveHeight: true,
-    //     draggable: true,
-    //     swipe: true,
-    // });
+    $('.galleries-img').magnificPopup({
+        type: 'image',
+        mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+
+        zoom: {
+            enabled: true,
+
+            duration: 300,
+            easing: 'ease-in-out',
+
+
+            function(openerElement) {
+                return openerElement.is('img') ? openerElement : openerElement.find('img');
+            }
+        }
+
+    });
 
     $('.booking-types').slick({
         lazyLoad: 'ondemand',
@@ -17,6 +25,13 @@ $(document).ready(function (){
         draggable: true,
         swipe: true,
     });
+
+    $('.galleries').slick({
+        dots: true,
+        draggable: true,
+        swipe: true,
+    });
+
 
 })
 
